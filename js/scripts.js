21eb440f809d7bc4d87f7475b3935aa7980efefc
Joseph Adediji <josylado@gmail.com>
2019-07-11 10:50:45 +0300

    add more functions to JS file

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,30 @@ var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 var result = add(number1, number2);
 alert(result);
+
+var subtract = function(number3, number4) {
+  return number3 - number4;
+};
+
+var number3 = parseInt(prompt("Enter a number:"));
+var number4 = parseInt(prompt("Enter another number:"));
+var result = subtract(number3, number4);
+alert(result);
+
+var subtract = function(number3, number4) {
+  return number3 * number4;
+};
+
+var number3 = parseInt(prompt("Enter a number:"));
+var number4 = parseInt(prompt("Enter another number:"));
+var result = subtract(number3, number4);
+alert(result);
+
+var subtract = function(number3, number4) {
+  return number3 / number4;
+};
+
+var number3 = parseInt(prompt("Enter a number:"));
+var number4 = parseInt(prompt("Enter another number:"));
+var result = subtract(number3, number4);
+alert(result);
